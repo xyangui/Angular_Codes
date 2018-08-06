8a78fe2e8d486065c26898dea82a57667e133981
xyangui <xyangui@163.com>
2018-08-06 16:16:14 +1000

    update unit test

--- No010-TourofHeroes6/src/app/app.component.spec.ts
+++ No010-TourofHeroes6/src/app/app.component.spec.ts
@@ -1,11 +1,30 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import {AppRoutingModule} from "./app-routing.module";
+import {DashboardComponent} from "./dashboard.component";
+import {HeroDetailComponent} from "./hero-detail.component";
+import {HeroesComponent} from "./heroes.component";
+import {HeroSearchComponent} from "./hero-search.component";
+import {HeroService} from "./hero.service";
+
+import { APP_BASE_HREF } from '@angular/common';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
+      imports: [
+        FormsModule,
+        AppRoutingModule,
       ],
+      declarations: [
+        AppComponent,
+        DashboardComponent,
+        HeroDetailComponent,
+        HeroesComponent,
+        HeroSearchComponent
+      ],
+      providers: [HeroService, { provide: APP_BASE_HREF, useValue : '/' }],
     }).compileComponents();
   }));
   it('should create the app', async(() => {
@@ -16,12 +35,12 @@ describe('AppComponent', () => {
   it(`should have as title 'app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('Tour of Heroes');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to No1-HelloWorld!');
+    expect(compiled.querySelector('h1').textContent).toContain('Heroes');
   }));
 });
